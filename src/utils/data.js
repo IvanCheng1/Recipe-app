666b5710906dc8efb4f12212201c361e6a8748fc
Ivan Cheng <ivan@ivancheng.co.uk>
2020-08-05 15:42:26 +0100

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,34 +3,74 @@ export const recipes = {
     title: "Edamame Bean Pasta",
     course: "main",
     ingredients: {
-      Spices: {
-        'curry powder': "200g",
-        'pepper': "1 tbsp",
-        "rapeseed oil": "2 tbsp",
-        "peanut butter": "6 tbsp",
-        "soy sauce": "1 tbsp",
-        "veg stock cube": "1",
-      },
+      Spices: [
+        {
+          item: 'curry powder',
+          quantity: "200g",
+        },
+        {
+          item: 'pepper',
+          quantity: "1 tbsp",
+        },
+        {
+          item: 'rapeseed oil',
+          quantity: "2 tbsp",
+        },
+        {
+          item: 'peanut butter',
+          quantity: "6 tbsp",
+        },
+        {
+          item: 'soy sauce',
+          quantity: "1 tbsp",
+        },
+        {
+          item: 'veg stock cube',
+          quantity: "1",
+        },
+      ],
       Colds: {
 
       },
-      Vegetables: {
-        "tomatoes": "6",
-        "onions": "2",
-        "garlic": "2 cloves",
-        "red chilli": "1",
-        "spring onions": "bunch"
-      },
-      Frozen: {
-        "edamame beans": "3 mugs"
-      },
-      Dry: {
-        "pasta": "500g",
-        "cornflour": "1 tbsp"
-      },
-      Other: {
-
-      }
+      Vegetables: [
+        {
+          item: 'tomatoes',
+          quantity: "6",
+        },
+        {
+          item: 'onions',
+          quantity: "2",
+        },
+        {
+          item: 'garlic',
+          quantity: "2 cloves",
+        },
+        {
+          item: 'red chilli',
+          quantity: "1",
+        },
+        {
+          item: 'spring onions',
+          quantity: "bunch",
+        },
+      ],
+      Frozen: [
+        {
+          item: 'edamame beans',
+          quantity: "3 mugs",
+        },
+      ],
+      Dry: [
+        {
+          item: 'pasta',
+          quantity: "500g",
+        },
+        {
+          item: 'cornflour',
+          quantity: "1 tbsp",
+        },
+      ],
+      Other: []
     },
     notes: "Not too difficult, but takes a while to make",
     image: "https://image.shutterstock.com/z/stock-photo-italian-cuisine-edamame-and-mentaiko-pasta-on-white-background-128504978.jpg",
@@ -39,109 +79,69 @@ export const recipes = {
     title: "Katsu Curry",
     course: "main",
     ingredients: {
-      Spices: {
-        'curry powder': "222g",
-        'pepper': "1 tbsp",
-      },
-      Colds: {
-      },
-      Vegetables: {
-        "Cauliflower": "250g",
-      },
-      Frozen: {
-
-      },
-      Dry: {
-        "rice": "500g",
-        "cornflour": "1 tbsp"
-      },
-      Other: {
-        
-      }
+      Spices: [
+        {
+          item: 'curry powder',
+          quantity: "222g",
+        },
+        {
+          item: 'pepper',
+          quantity: "1 tbsp",
+        },
+      ],
+      Colds: [],
+      Vegetables: [
+        {
+          item: 'cauliflower',
+          quantity: "222g",
+        },
+      ],
+      Frozen: [],
+      Dry: [
+        {
+          item: 'rice',
+          quantity: "500g",
+        },
+        {
+          item: 'cornflour',
+          quantity: "1 tbsp",
+        },
+      ],
+      Other: []
     },
     notes: "Takes a lot of effort",
     image: "https://image.shutterstock.com/z/stock-photo-rice-with-deep-fried-pork-and-curry-in-japanese-style-or-katsu-kare-in-white-plate-ready-to-eat-665181721.jpg",
-  },
-  RisottoAndMushrooms: {
-    title: "Risotto and Mushrooms",
-    course: "main",
-    ingredients: {
-      Spices: {
-        'curry powder': "222g",
-        'pepper': "1 tbsp",
-      },
-      Colds: {
-      },
-      Vegetables: {
-        "Cauliflower": "250g",
-        "broccoli": "1kg",
-      },
-      Frozen: {
-        "edamame beans": "3 mugs"
-      },
-      Dry: {
-        "pasta": "500g",
-        "cornflour": "1 tbsp"
-      },
-      Other: {
-        
-      }
-    },
-    notes: "",
-    image: "https://image.shutterstock.com/z/stock-photo-frying-pan-with-risotto-and-mushrooms-on-wooden-table-1122530729.jpg",
-  },
-  SpaghettiBolognese: {
-    title: "Spaghetti Bolognese",
-    course: "main",
-    ingredients: {
-      Spices: {
-        'curry powder': "222g",
-        'pepper': "1 tbsp",
-      },
-      Colds: {
-      },
-      Vegetables: {
-        "peppers": "3",
-      },
-      Frozen: {
-        "peas": "2 mugs"
-      },
-      Dry: {
-        "spaghetti": "500g",
-        "cornflour": "1 tbsp",
-        "chopped tomatoes": "1"
-      },
-      Other: {
-        
-      }
-    },
-    notes: "Super easy",
-    image: "https://image.shutterstock.com/z/stock-photo-spaghetti-pasta-with-bolognese-sauce-and-parmesan-cheese-top-view-534149023.jpg",
   },
   LentilRagu: {
     title: "Lentil Ragu",
     course: "main",
     ingredients: {
-      Spices: {
-        'curry powder': "222g",
-        'pepper': "1 tbsp",
-      },
-      Colds: {
-
-      },
-      Vegetables: {
-        "carrots": "2",
-      },
-      Frozen: {
-
-      },
-      Dry: {
-        "chopped tomatoes": "1",
-        "passata": "1"
-      },
-      Other: {
-        
-      }
+      Spices: [
+        {
+          item: 'pepper',
+          quantity: "1 tbsp",
+        },
+      ],
+      Colds: [],
+      Vegetables: [
+        {
+          item: 'carrots',
+          quantity: "2",
+        },
+      ],
+  
+      Frozen: [],
+      Dry: [
+        {
+          item: 'chopped tomatoes',
+          quantity: "1",
+        },
+        {
+          item: 'passata',
+          quantity: "1",
+        },
+      ],
+      Other: []
     },
     notes: "Slow cooker for 5h on low",
     image: "https://image.shutterstock.com/z/stock-photo-lentil-ragu-vegan-lentil-bolognese-in-a-pan-rustic-food-1335686036.jpg",
